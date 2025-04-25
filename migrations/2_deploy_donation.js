feef8880c362383f1dc8d81403f5d516385088a1
Jason Dsouza <jasmcaus@gmail.com>
2025-04-25 00:33:21 -0700

    update

--- migrations/2_deploy_donation.js
+++ migrations/2_deploy_donation.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const path = require('path');
 const EnergyToken = artifacts.require("EnergyToken");
 const EnergyMarketplace = artifacts.require("EnergyMarketplace");
 const RenewableEnergyCertification = artifacts.require("RenewableEnergyCertification");
@@ -22,6 +24,41 @@ module.exports = async function(deployer, network, accounts) {
   console.log("Updating market controller in EnergyToken...");
   await energyToken.changeMarketController(energyMarketplace.address);
   console.log("Market controller updated");
+
+  // Write contract addresses to file
+  const contracts = {
+    EnergyToken: energyToken.address,
+    EnergyMarketplace: energyMarketplace.address,
+    RenewableEnergyCertification: renewableCertification.address
+  };
+
+  const clientAppDir = path.join(__dirname, '../client/src/app');
+  
+  // Ensure directory exists
+  if (!fs.existsSync(clientAppDir)){
+    fs.mkdirSync(clientAppDir, { recursive: true });
+  }
+
+  // Write contract addresses
+  const contractsPath = path.join(clientAppDir, 'contracts.json');
+  fs.writeFileSync(contractsPath, JSON.stringify(contracts, null, 2));
+  console.log("Contract addresses written to:", contractsPath);
+
+  // Copy contract JSON files from build
+  const buildDir = path.join(__dirname, '../build/contracts');
+  fs.copyFileSync(
+    path.join(buildDir, 'EnergyToken.json'),
+    path.join(clientAppDir, 'EnergyToken.json')
+  );
+  fs.copyFileSync(
+    path.join(buildDir, 'EnergyMarketplace.json'),
+    path.join(clientAppDir, 'EnergyMarketplace.json')
+  );
+  fs.copyFileSync(
+    path.join(buildDir, 'RenewableEnergyCertification.json'),
+    path.join(clientAppDir, 'RenewableEnergyCertification.json')
+  );
+  console.log("Contract ABIs copied to client directory");
   
   console.log("Deployment complete!");
 };
